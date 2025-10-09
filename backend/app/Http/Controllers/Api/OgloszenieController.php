b7b25537a713547ab3e9ed6231d6498b598c92cf
SzymonPaw <szymonpaw01@gmail.com>
2025-10-09 23:01:54 +0200

    Optymalizacja

--- backend/app/Http/Controllers/Api/OgloszenieController.php
+++ backend/app/Http/Controllers/Api/OgloszenieController.php
@@ -10,11 +10,15 @@ class OgloszenieController extends Controller
     public function index()
     {
         // Pobierz 10 najnowszych ogłoszeń
-        $ogloszenia = Ogloszenie::with(['marka','modelPojazdu','zdjecia'])
+        $ogloszenia = Ogloszenie::with(['marka', 'modelPojazdu', 'zdjecia'])
             ->orderBy('created_at', 'desc')
             ->take(10)
             ->get();
 
-        return response()->json($ogloszenia);
+        return response()
+            ->json($ogloszenia)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
