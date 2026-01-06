b13f55f495ef71b31ed04a76ad7ba972afe81f14
Szymon Paw <szymonpaw01@gmail.com>
2026-01-06 02:56:21 +0100

    Remove frontend files and documentation for AutoTablica project

--- kopia/frontend/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders api tester heading', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /autotablica api tester/i });
-  expect(heading).toBeInTheDocument();
-});
